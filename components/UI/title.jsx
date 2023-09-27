b5ee8a7fbaf2f147f3d2ac06c254bac716db8826
sudeakq <sudeakq@gmail.com>
2023-09-28 01:38:43 +0300

    add search modal component

---
+++ components/UI/title.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+const Title = ({ children, addClass }) => {
+    return (
+        <div className={`${addClass}`}>{children}</div>
+    )
+}
+
+export default Title;
--- components/layout/header.jsx
+++ components/layout/header.jsx
@@ -1,57 +1,53 @@
-import Logo from "../UI/logo";
-import React from "react";
+import { useState } from "react";
 import { FaUserAlt, FaShoppingCart, FaSearch } from "react-icons/fa";
+import OutsideClickHandler from "react-outside-click-handler";
+import Logo from "../ui/Logo";
+import Title from "../ui/Title";
+import Search from './../UI/Search';
 
 const Header = () => {
+    const [isSearchModal, setIsSearchModal] = useState(false);
+
     return (
-        <div className="h-[5.5rem] bg-secondary flex justify-between">
-
-            <div className="container mx-auto text-white flex justify-between h-full">
-                <div>
-                    <Logo />
-                </div>
-
-                <div>
-                    <nav>
-                        <ul className="flex gap-x-2">
-                            <li className="px-[5px] py-[20px] uppercase
-                            hover:text-primary cursor-pointer">
-                                <a href="">Home</a>
-                            </li>
-                            <li className="px-[5px] py-[20px] uppercase
-                            hover:text-primary cursor-pointer">
-                                <a href="">Menu</a>
-                            </li>
-                            <li className="px-[5px] py-[20px] uppercase
-                            hover:text-primary cursor-pointer">
-                                <a href="">About</a>
-                            </li>
-                            <li className="px-[5px] py-[20px] uppercase
-                            hover:text-primary cursor-pointer">
-                                <a href="">Book Table</a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-
+        <div className="h-[5.5rem] bg-secondary">
+            <div className="container mx-auto text-white flex justify-between items-center h-full">
+                <Logo />
+                <nav>
+                    <ul className="flex gap-x-2">
+                        <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+                            <a href="">Home</a>
+                        </li>
+                        <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+                            <a href="">Menu</a>
+                        </li>
+                        <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+                            <a href="">About</a>
+                        </li>
+                        <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+                            <a href="">Book Table</a>
+                        </li>
+                    </ul>
+                </nav>
                 <div className="flex gap-x-4 items-center">
-                    <a href="">
-                        <FaUserAlt />
+                    <a href="#">
+                        <FaUserAlt className="hover:text-primary transition-all" />
                     </a>
-                    <a href="">
-                        <FaShoppingCart />
+                    <a href="#">
+                        <FaShoppingCart className="hover:text-primary transition-all" />
                     </a>
-                    <a href="">
-                        <FaSearch />
-                    </a>
-                    <a href="">
+                    <button onClick={() => setIsSearchModal(true)}>
+                        <FaSearch className="hover:text-primary transition-all" />
+                    </button>
+                    <a href="#">
                         <button className="btn-primary">Order Online</button>
                     </a>
-
                 </div>
             </div>
-
+            {isSearchModal && (
+                <Search setIsSearchModal={setIsSearchModal} />
+            )}
         </div>
-    )
-}
+    );
+};
+
 export default Header;
